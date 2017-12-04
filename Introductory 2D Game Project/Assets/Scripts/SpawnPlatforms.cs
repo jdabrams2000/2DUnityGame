49d4fa71091345f919238a9758584942bff33571
jdabrams2000 <abramsj@bxscience.edu>
2017-12-03 22:48:49 -0500

    Add 2D Unity Game

---
+++ Introductory 2D Game Project/Assets/Scripts/SpawnPlatforms.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnPlatforms : MonoBehaviour {
+
+	public float x;
+	public float dX;
+	private float posX;
+	private float posY;
+
+	// Use this for initialization
+	void Start () {
+		transform.position = new Vector2 (x, 0);
+		posX = x;
+		posY = 0;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (posX <= -4f) {
+			posY = Random.Range (-3.2f,4.2f);
+			posX = 4f;
+		}
+		else {
+			posX = posX - dX;
+		}
+		transform.position = new Vector2 (posX, posY);
+	}
+}
---
+++ Introductory 2D Game Project/Assets/Scripts/SpawnScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnScript : MonoBehaviour {
+
+	public GameObject[] obj;
+	public float spawnMin = 1f;
+	public float spawnMax = 2f;
+	public Vector2 pos = new Vector2(-4,0);
+	public float size = 1.0f;
+	public Vector2 dir = Vector2.right;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (Spawn ());
+	}
+	
+	IEnumerator Spawn() {
+		while (true) {
+			Instantiate(obj [Random.Range(0,obj.Length)], pos, Quaternion.identity);
+			pos += dir * size;
+			yield return new WaitForSeconds(Random.Range(spawnMin, spawnMax));
+		}
+	}
+}
